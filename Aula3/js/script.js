d943b78ce2ba34b2ca8ba12c9466ec13177a6474
Karina <ka_fraga@hotmail.com>
2020-09-26 19:36:35 -0300

    Desafio e final módulo 1

--- Aula3/js/script.js
+++ Aula3/js/script.js
@@ -4,5 +4,27 @@
 //   currentElement.classList.add('emphasis');
 // }
 
-var input = document.querySelector('#input1');
-input.value = 'Karina Fraga';
+var nameInput = document.querySelector('#nameInput');
+nameInput.addEventListener('keyup', countName);
+
+var form = document.querySelector('form');
+form.addEventListener('submit', preventSubmit);
+
+start();
+
+function start() {
+  console.log('Todo o Dom foi carregado');
+}
+
+function countName(event) {
+  var count = event.target.value;
+  var span = document.querySelector('#nameLength');
+  span.textContent = count.length;
+}
+
+function preventSubmit(event) {
+  event.preventDefault();
+
+  var nameInput = document.querySelector('#nameInput');
+  alert(nameInput.value + 'cadastrado com sucesso!');
+}
