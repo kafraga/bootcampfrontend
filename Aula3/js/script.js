4034b21032a5af14fbca36260af5462a3ee45d2f
Karina <ka_fraga@hotmail.com>
2020-09-26 16:31:16 -0300

    Aula de Form

--- Aula3/js/script.js
+++ Aula3/js/script.js
@@ -1,5 +1,8 @@
-var data = Array.from(document.querySelectorAll('.data'));
-for (var i = 0; i < data.length; i++) {
-  var currentElement = data[i];
-  currentElement.classList.add('emphasis');
-}
+// var data = Array.from(document.querySelectorAll('.data'));
+// for (var i = 0; i < data.length; i++) {
+//   var currentElement = data[i];
+//   currentElement.classList.add('emphasis');
+// }
+
+var input = document.querySelector('#input1');
+input.value = 'Karina Fraga';
